77e7032c17d26c6541ddfdbd26acf48115a7f596
guy <guy.duenias@post.runi.ac.il>
2024-03-17 19:08:08 +0200

    checking and debuging

--- q3.sh
+++ q3.sh
@@ -4,10 +4,10 @@
 echo -e "Lets play WORDLE! \nplease enter a 5 letter word"
 
 #getting letters input
-read letters
+read -r letters
 
 # Checking the length of the word
-if [ ${#letters} -ne 5 ]; then
+if [ "${#letters}" -ne 5 ]; then
         echo "Error: not a 5 letter word"
         exit 1
 fi
@@ -16,8 +16,9 @@ echo "Fantastic! now what are the colors of the letters?"
 echo -e "Remember, \n's' is for silver (not in the word), \n'y' is for yellow (in the word, wrong place) and \n'g' for green (in the word and in the right place)"
 
 #getting colors input
-read colors
+read -r colors
 
+# making sure its all lower case letters
 colors=$(echo "$colors" | tr '[:upper:]' '[:lower:]')
 
 # Sort the characters of the string colors
@@ -33,54 +34,40 @@ if ! [[ $sorted_string =~ $pattern ]]; then
 fi
 
 # Bank of 5 letter Words
-words_dict=$(<"test")
-echo "$words_dict"
+words_dict=$(<sgb-words.txt)
 
-# Main filtering of the words bank by the critiria given
-for i in {0..4}; do
+# initialize filtered_words with the entire word list
+filtered_words="$words_dict"
+echo "$filtered_words run 0"
+
+# main filtering of the words bank by the critiria given
+for ((i = 0; i < 5; i++)); do
         #takes the letter and the color
         letter=${letters:$i:1}
         color=${colors:$i:1}
-	echo "$letter" "$color"
-	#checks for first run or not
-	if [ $i -eq 0 ]; then
-		#filter by color
-	        if [ $color = 'g' ]; then
-	                # green- in the right place
-	                #take all words that contain the letter in the i posotion
-	                filtered_words=$(awk -v letter="$letter" pos="$((i+1))" 'length($0)==5 && substr($0, pos, 1)==letter' <<< "$words_dict")
-	        elif [ $color = 's' ];then
-	                # silver not in the word at all
-	                #take all words that doesnt contain the letter
-	                filtered_words=$(awk -v letter="$letter" 'index($0,letter) == 0' <<< "$words_dict")
-	        elif [ $color = 'y' ]; then
-	                # yellow- not right place
-	                # taking all words that doesnt have the letter in position i
-	                filtered_words=$(awk -v letter="$letter" -v pos="$((i+1))" 'length($0)==5 && substr($0, pos, 1) != letter' <<<"$words_dict")
-	                # taking all the words that contain same letter in other places
-	                filtered_words=$(awk -v letter="$letter" 'index($0,letter) > 0' <<< "$words_dict")
-	        fi
-	else
-		#works on filtered_words and not the main bank of words
-		#filter by color
-                if [ $color = 'g' ]; then
-                        # green- in the right place
-                        #take all words that contain the letter in the i posotion
-                        filtered_words=$(awk 'length($0)==5 && substr($0, '"$((i+1))"', 1)=="'"$letter"'"' <<< "$filtered_words")
-                elif [ $color = 's' ];then
-                        # silver not in the word at all
-                        #take all words that doesnt contain the letter
-                        filtered_words=$(awk -v letter="$letter" 'index($0,letter) == 0' <<< "$filtered_words")
-                elif [ $color = 'y' ]; then
-                        # yellow- not right place
-                        # taking all words that doesnt have the letter in position i
-                        filtered_words=$(awk -v letter="$letter" -v pos="$i" 'length($0)==5 && substr($0, pos, 1) != letter' <<< "$filtered_words")
-                        # taking all the words that contain same letter in other places
-                        filtered_words=$(awk -v letter="$letter" 'index($0,letter) > 0' <<< "$filtered_words")
-                fi
 
-	fi
+	#works on filtered_words and not the main bank of words
+	#filter by color
+        if [ "$color" = 'g' ]; then
+                # green- in the right place
+                #take all words that contain the letter in the i posotion
+                filtered_words=$(awk -v letter="$letter" -v pos="$((i + 1))" 'length($0)==5 && substr($0, pos, 1)==letter' <<<"$filtered_words")
+		echo "$filtered_words green"
+        elif [ "$color" = 's' ];then
+                # silver not in the word at all
+                #take all words that doesnt contain the letter
+                filtered_words=$(awk -v letter="$letter" 'index($0, letter) == 0' <<<"$filtered_words")
+		echo "$filtered_words silver"
+        elif [ "$color" = 'y' ]; then
+                # yellow- not right place
+                # taking all words that doesnt have the letter in position i
+                temp_words=$(awk -v letter="$letter" -v pos="$((i + 1))" 'length($0)==5 && substr($0, pos, 1) != letter' <<<"$filtered_words")
+		echo "$filtered_words yellow 1"
+                # taking all the words that contain same letter in other places
+                filtered_words=$(awk -v letter="$letter" 'index($0, letter) > 0' <<<"$temp_words")
+		echo "$filtered_words yellow 2"
+        fi
 done
 echo "The words that match the description:"
-echo "$words_dict"
+echo "$filtered_words"
 
