b44abaf798d7a82a2067a4b4edf1b57fd7e7f360
guy <guy.duenias@post.runi.ac.il>
2024-03-17 12:28:23 +0200

    first attemt regex

--- q3.sh
+++ q3.sh
@@ -7,8 +7,8 @@ read letters
 
 # Checking the length of the word
 if [ ${#letters} -ne 5 ]; then
-	echo "Error: not a 5 letter word"
-	exit 1
+        echo "Error: not a 5 letter word"
+        exit 1
 fi
 
 echo "Fantastic! now what are the colors of the letters?"
@@ -26,11 +26,33 @@ pattern="^[sgy]+$"
 
 # Check if the sorted string matches the pattern
 if ! [[ $sorted_string =~ $pattern ]]; then
-	echo "Error: string '$colors' contain other characters than 's', 'g' and 'y'"
-	exit 1
+        echo "Error: string '$colors' contain other characters than 's', 'g' and 'y'"
+        exit 1
 fi
 
 # Bsnk of 5 letter Words
-words_file=$(<"sgb-words.txt")
-echo "$words_file" | head -10
+words_dict=$(<"sgb-words.txt")
+
+# Main filtering of the words bank by the critiria given
+for i in {0..4}; do
+        #takes the letter and the color
+        letter=${letters:$i:1}
+        color=${colors:$i:1}
+
+        #filter by color
+        if [ $color = 'g' ]; then
+                # green- in the right place
+                filtered_words=grep "^.\{$i\}$letter.\{4-$i\}$" "$words_dict"
+        elif [ $color = 's' ];then
+                # silver not in the word at all
+		filtered_words=grep -v '^[^$letter]*$' "$words_dict.txt"
+        else
+                # yellow- not right place
+	        filtered_words=grep -v "^.\{$i\}$letter.\{4-$i\}$" "$words_dict"
+        fi
+
+done
+
+echo "The words that match the description:"
+echo $filtered_words
 
